6ecc3def9f1312045cfae0858d322d2b1fe97f41
Deepanshu Chaudhary <deepanshuchoudhary990@gmail.com>
2025-07-08 15:21:47 +0530

    improved login page UI

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,25 +1,56 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
-    <div className="addUser">
+    <div>
+      <div className="container py-3"></div>
+      <div className="container col-md-4 card rounded-7 me-lg-n5 py-3">
         <h3>Sign In</h3>
-        <form className="addUserForm">
-            <div className="inputGroup">
-                <label htmlFor="email">Email</label>
-                <input type="email" id="email" placeholder="Enter your email" autoComplete="off"/>
-                <label htmlFor="username">Password</label>
-                <input type="password" id="password" placeholder="Enter your password" autoComplete="off"/>
-                <Link to="/home" type="submit" class="btn btn-outline-primary">Login</Link>
+        <form>
+          <div className="row">
+            <div className="mb-3 col-sm-12">
+              <label for="email" className="form-label">
+                Email Address
+              </label>
+              <input
+                type="email"
+                className="form-control"
+                id="exampleinputemail2"
+                aria-describedby="emailHelp"
+              />
+              <div id="emailHelp" className="form-text">
+                We'll never share your email with anyone else.
+              </div>
             </div>
+            <div className="mb-3">
+              <label for="password" className="form-label">
+                Password
+              </label>
+              <input type="password" id="password" className="form-control" />
+            </div>
+            <div className="mb-3">
+              <button
+                to="/home"
+                type="submit"
+                class="btn btn-outline-primary col-12"
+              >
+                Login
+              </button>
+            </div>
+          </div>
         </form>
         <div className="signUp">
-            <p>Don't have an account?</p>
-            <Link to="/" type="submit" class="btn btn-outline-primary">Sign Up</Link>
+          <p>
+            Don't have an account?{" "}
+            <Link to="/" type="submit" className="">
+              Sign Up
+            </Link>
+          </p>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Login;
--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,46 +1,80 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Signup = () => {
   return (
     <div>
       <div className="container py-3"></div>
       <div className="container col-md-4 card rounded-7 me-lg-n5 py-3">
-          <h3>Sign Up</h3>
-          <form>
-              <div className="row">
-                  <div className="mb-3 col-sm-12">
-                    <label for="username" className="form-label">Username</label>
-                    <input type="username" className="form-control" id="username" aria-describedby="emailHelp" />
-                  </div>
-                  <div className="mb-3 col-sm-12">
-                    <label for="email" className="form-label">Email address</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-                  </div>
-                  <div className="mb-3">
-                    <label for="password" className="form-label">Password</label>
-                    <input type="password" className="form-control" id="password" />
-                  </div>
-
-                  <div className="mb-3">
-                    <div className="form-check">
-                      <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                      <label className="form-check-label" for="exampleCheck1">Terms and Conditions</label>
-                    </div>
-                  </div>
-                  <div class="mb-3">
-                    <button to="/login" type="submit" className="btn btn-outline-primary col-12">Sign Up</button>
-                  </div>
+        <h3>Sign Up</h3>
+        <form>
+          <div className="row">
+            <div className="mb-3 col-sm-12">
+              <label for="username" className="form-label">
+                Username
+              </label>
+              <input
+                type="username"
+                className="form-control"
+                id="username"
+                aria-describedby="emailHelp"
+              />
+            </div>
+            <div className="mb-3 col-sm-12">
+              <label for="email" className="form-label">
+                Email address
+              </label>
+              <input
+                type="email"
+                className="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+              />
+              <div id="emailHelp" className="form-text">
+                We'll never share your email with anyone else.
               </div>
-          </form>
-          <div className="login">
-              <p>Already have an account?  <Link to="/login" type="submit" className="">Login</Link>
-              </p>
+            </div>
+            <div className="mb-3">
+              <label for="password" className="form-label">
+                Password
+              </label>
+              <input type="password" className="form-control" id="password" />
+            </div>
+
+            <div className="mb-3">
+              <div className="form-check">
+                <input
+                  type="checkbox"
+                  className="form-check-input"
+                  id="exampleCheck1"
+                />
+                <label className="form-check-label" for="exampleCheck1">
+                  Terms and Conditions
+                </label>
+              </div>
+            </div>
+            <div className="mb-3">
+              <button
+                to="/login"
+                type="submit"
+                className="btn btn-outline-primary col-12"
+              >
+                Sign Up
+              </button>
+            </div>
           </div>
+        </form>
+        <div className="login">
+          <p>
+            Already have an account?{" "}
+            <Link to="/login" type="submit" className="">
+              Login
+            </Link>
+          </p>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Signup;
